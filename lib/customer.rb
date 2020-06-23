b05b694e8940d372117753abc5b1f02b8a3704d2
brody sullivan <brodysullivan0@gmail.com>
2020-06-23 16:13:29 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
   
+  attr_accessor :name, :age 
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
 end
+
+sam = Customer.new("Sam", 27)
+  
+  sam.new_meal(pat, 50, 10)
+  sam.new_meal(alex, 20, 3)
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
 
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+  
+  def initialize(name, yrs_experience)
+    @name = name 
+    @yrs_experience = yrs_experience
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
 end
+
+pat = Waiter.new("Pat", 2)
+  alex = Waiter.new("Alex", 5)
+  
+  pat.new_meal(sam, 30, 5)
