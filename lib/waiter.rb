b7b9fbce2bdd089cd82a7a967d3bd131432976c5
brody sullivan <brodysullivan0@gmail.com>
2020-06-23 16:18:06 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,7 +14,11 @@ class Waiter
     @@all 
   end
   
-  
+  def meals
+  Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
+  end
+end
   
   def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
